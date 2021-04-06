2169e6292d95b02d1afaf832d3313acbde9c9de9
Bastian Radlmaier <bastian.radlmaier@code.berlin>
2021-04-06 14:59:36 +0200

    test

--- color_check/website.py
+++ color_check/website.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from controllers import get_color_code
+from color_check.controllers.get_color_code import get_color_code
 app = Flask(__name__)
 
 
